3a0b2cf5ed438e14444fb56bc802cba22572ded0
风踪枫叶 <leon@baobaoj.com>
2014-02-10 17:12:25 +0800

    new template. :)

---
+++ js/parallaxInit.js
@@ -0,0 +1,42 @@
+// PARALLAX CALLING
+//====================================================================
+//====================================================================
+( function ( $ ) {
+'use strict';
+$(document).ready(function(){
+$(window).bind('load', function () {
+		parallaxInit();						  
+	});
+	function parallaxInit() {
+		testMobile = isMobile.any();
+		if (testMobile == null)
+		{
+			$('.twitter-teaser-bg').parallax("50%", 0.5);
+			$('.banner').parallax("50%", 0.5);
+		}
+	}	
+	parallaxInit();	 
+});	
+//Mobile Detect
+var testMobile;
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+}( jQuery ));
